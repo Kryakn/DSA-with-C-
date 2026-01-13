503bf7be8d60640c3f4b4d7c27ca0524351ccfd2
Kryakn <aryanshrivastav150@gmail.com>
2026-01-13 16:21:56 +0530

    SI formula

--- lecture_1/Addtwonumbers.cpp
+++ lecture_1/Addtwonumbers.cpp
@@ -3,6 +3,8 @@ using namespace std;
 int main(){
     int a=10,b=20;
     int c=a+b;
-    cout<<"Sum"<<c;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<"Sum"<<c<<endl;
     return 0;
 }
---
+++ lecture_1/SIformula.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int P,R,T,SI;
+    SI=(P*R*T)/100;
+    cout<<"Principle amount"<<endl;
+    cin>>P;
+    cout<<"Rate"<<endl;
+    cin>>R;
+    cout<<"Time"<<endl;
+    cin>>T;
+    cout<<"Simple Intrest"<<SI<<endl;
+return 0;
+
+}
