ce06debc38bd6e45be39113390418d5571b0bf92
Kryakn <aryanshrivastav150@gmail.com>
2026-01-14 11:47:33 +0530

    OddEvennumbers

---
+++ lecture_1/oddeven.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Number"<<endl;
+    cin>>num;
+    if(num%2==0){
+        cout<<"number is even"<<num<<endl;
+
+    }else{
+        cout<<"number is odd"<<endl;
+    }
+    return 0;
+    
+}
