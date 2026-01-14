25dc5c66f51e5b8f56390c3e7820550214dc10f5
Kryakn <aryanshrivastav150@gmail.com>
2026-01-14 12:32:06 +0530

    print 1 to n

---
+++ lecture_1/print1toN.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"value of number"<<endl;
+    cin>>n;
+    cout<<"OUTPUT"<<endl;
+    int i = 1;
+while (i <= n) {
+    cout << i << ",";
+    i++;
+}
+    return 0;
+}
