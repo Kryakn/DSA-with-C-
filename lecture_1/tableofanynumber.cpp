6f215ec01f9428fc2b9329ef626b914e53788e89
Kryakn <aryanshrivastav150@gmail.com>
2026-01-14 12:49:31 +0530

    table of number

---
+++ lecture_1/tableofanynumber.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{int n;
+    cout<<"Give the number of which table you want"<<endl;
+    cin>>n;
+    for (int i = 1; i <= 10; i++) {
+        cout << n<<" x " << i << " = " << n * i << endl;
+    }
+    return 0;
+}
