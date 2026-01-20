d996dc65293ced15ae32cf84ac47fe41ad90cef5
Kryakn <aryanshrivastav150@gmail.com>
2026-01-20 15:51:57 +0530

    leftsidefaced symmetric pattern

---
+++ lecture_4/rowsymmetricleftsideface.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,j,i;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n-i;j++){
+            cout<<"  ";
+        }
+        for(j=1;j<=i;j++){
+            cout<<"* ";
+
+        }
+       cout<<endl;
+    }
+   for(i=n-1;i>=1;i--)
+    {
+        for(j=1;j<=n-i;j++){
+            cout<<"  ";
+        }
+        for(j=1;j<=i;j++){
+            cout<<"* ";
+
+        }
+       cout<<endl;
+}    
+    
+    return 0;
+}
