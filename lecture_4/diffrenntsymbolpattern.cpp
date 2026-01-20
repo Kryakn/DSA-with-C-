131a450a365167ca1c14770a5ea4d52d82069d93
Kryakn <aryanshrivastav150@gmail.com>
2026-01-20 15:11:05 +0530

    different symbol pattern

---
+++ lecture_4/diffrenntsymbolpattern.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,n;
+    cin>>n;
+
+    for(i=1;i<=n;i++)
+    {
+        //spaces
+        for(j=1;j<=n-i;j++){
+            cout<<"  ";
+        }
+        //total pattern
+        for(j=1;j<=(2*i-1);j++){
+            if(j%2==0){
+                //!
+                cout<<"! ";
+
+            }
+            else{
+                //star
+                cout<<"* ";
+            }
+
+        }
+        cout<<endl;
+    }
+    return 0;
+}
