c5d2d4e87b07fbca4939462deda5b082cbdf4692
Kryakn <aryanshrivastav150@gmail.com>
2026-01-24 12:39:12 +0530

    feat: add number pattern with asterisk

---
+++ lecture_6/pattern_5.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i,j;
+    cin>>n;
+    for(i=1;i<=n;i++){
+        for(j=n;j>=1;j--){
+            if(i==j){
+                cout<<"* ";
+                }
+                else{
+                    cout<<j<<" ";
+                }
+        
+        }
+        cout<<endl;
+    }
+    return 0;
+}
