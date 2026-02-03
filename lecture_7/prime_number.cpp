90e61acd1b825e357415f46cbe44de6f86cd6e3c
Kryakn <aryanshrivastav150@gmail.com>
2026-02-03 15:18:23 +0530

    normal program of prime number

---
+++ lecture_7/prime_number.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    bool ans=true;
+    // we are assuming earlier number is prime
+    for(int i=2;i<=n-1;i++){
+        if(n%i==0){
+            ans=false;
+
+        }
+    }
+        if(ans)
+        {
+            cout<<"prime";
+
+        }else{
+            cout<<"non prime";
+        }
+    
+    return 0;
+
+}
+/*
+concept of flag
+flag size=1byte
+*/
