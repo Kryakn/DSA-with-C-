f7f2473e8d13fc564a8cc097af72effdec222e08
Kryakn <aryanshrivastav150@gmail.com>
2026-02-04 13:48:14 +0530

    fibbonaci series by using function

---
+++ lecture_8/Fibbonacci_series.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int fibbo(int n){
+   if(n==0||n==1){
+    return n;
+   } 
+   //recursion
+   return fibbo(n-1)+fibbo(n-2);
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<fibbo(n)<<endl;
+    return 0;
+}
