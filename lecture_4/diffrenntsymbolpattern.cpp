afabf34c1ef922aeefad3cb3710b8dcc36e3d726
Kryakn <aryanshrivastav150@gmail.com>
2026-02-05 09:27:20 +0530

    logic patterns

--- lecture_4/diffrenntsymbolpattern.cpp
+++ lecture_4/diffrenntsymbolpattern.cpp
@@ -1,30 +1,39 @@
 #include<iostream>
 using namespace std;
-int main()
-{
-    int i,j,n;
-    if(!(cin>>n))
-
-    for(i=1;i<=n;i++)
-    {
-        //spaces
-        for(j=1;j<=n-i;j++){
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
             cout<<"  ";
         }
-        //total pattern
-        for(j=1;j<=(2*i-1);j++){
-            if(j%2==0){
-                //!
-                cout<<"! ";
-
-            }
-            else{
-                //star
+        for(int j=1;j<=i;j++){
+            if(j%2!=0){
                 cout<<"* ";
             }
-
+            else{
+                cout<<"! ";
+            }
+        }
+         for(int j=i-1;j>=1;j--){
+            if(j%2!=0){
+                cout<<"* ";
+            }
+            else{
+                cout<<"! ";
+            }
+        }
+        for(int j=1;j<=n-i;j++){
+            cout<<"  ";
         }
         cout<<endl;
     }
     return 0;
 }
+/*
+        *
+      * ! *       
+    * ! * ! *     
+  * ! * ! * ! *   
+* ! * ! * ! * ! * 
+*/
