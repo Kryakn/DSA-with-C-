b785f0a7dfc83ec21677d58a83c185a2b3722af6
Kryakn <aryanshrivastav150@gmail.com>
2026-03-19 23:52:44 +0530

    Leetcode 918 problem

---
+++ Leet_Code_problems/Leetcode_problem_number_918.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int m,n;
+    cin>>m>>n;
+    vector<vector<int>>mat(m,vector<int>(n));
+    for(int i=0;i<m;i++){
+          for(int j=0;j<n;j++){
+            cin>>mat[i][j];
+          }
+    }
+
+    bool up = true;
+    int i=0;
+    int r=0;int c=0;
+
+    vector<int>ans(m*n);
+
+    while(r<m && c<n){
+        if(up){
+            while(r>0 && c<n-1){
+                ans[i]=mat[r][c];
+                i++;
+                r--;
+                c++;
+            }
+            ans[i]=mat[r][c];
+            i++;
+            if(c==n-1){
+                r++;
+            }
+            else{
+                c++;
+            }
+        }
+        else{
+            while(c>0 && r<m-1){
+                ans[i]=mat[r][c];
+                i++;
+                r++;
+                c--;
+            }
+            ans[i]=mat[r][c];
+            i++;
+            if(r==m-1){
+                c++;
+            }
+            else{
+                r++;
+            }
+        
+        }
+        up=!up;
+    }
+    for(int i=0;i<ans.size();i++)
+    {
+        cout<<ans[i]<<" ";
+    }
+    return 0;
+}
