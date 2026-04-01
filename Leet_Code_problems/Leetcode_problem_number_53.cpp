bc60cc790d39b068d5dd9e330f4f21cd8e6272cb
Kryakn <aryanshrivastav150@gmail.com>
2026-04-01 23:10:03 +0530

    Leetcode problem53

---
+++ Leet_Code_problems/Leetcode_problem_number_53.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+    int maxSubArray(vector<int>& nums) {
+       int maxsum=nums[0];
+       int currentsum=0;
+       for(int i=0;i<nums.size();i++){
+             currentsum=currentsum+nums[i];
+
+        if(currentsum>maxsum){
+            maxsum=currentsum;
+
+        }
+        if(currentsum<0){
+            currentsum=0;
+        }
+       }
+       return maxsum;
+
+    }
+
+int main(){
+return 0;
+}
